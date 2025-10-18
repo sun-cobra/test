113354c8607c8612cea7fea5f9145c70526c2fa6
sun-cobra <sun-hy@dalian-it.com>
2025-10-18 13:49:00 +0800

    Add files via upload
    first time upload

---
+++ test.java
@@ -0,0 +1,10 @@
+class test {
+	public static void main(String args[]) {
+		String a = null;
+		if (a.equals("1")) {
+			System.out.println("true");
+		} else {
+			System.out.println("false");
+		}
+	}
+}
