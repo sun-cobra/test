ff098a9082bf7c5ea58e4885a95c31b9d85af4d3
sun-cobra <sun-hy@dalian-it.com>
2025-10-18 14:36:54 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 class test {
 	public static void main(String args[]) {
-		String a = null;
+		String a;
 		String b = "";
 		if (a.equals("1")) {
 			System.out.println("true");
@@ -9,4 +9,5 @@ class test {
 		}
 	}
 }
+
 
